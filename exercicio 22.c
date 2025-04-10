81a2237aabaea4461593a8a70ab4c7b95275e601
Frederico Modolo <fredericomodolo@gmail.com>
2025-04-10 16:53:41 -0300

    Delete exercicio 22.c

--- exercicio 22.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    float N, Nd;
-    printf("Digite o número de lados do polígono convexo: ");
-    scanf("%f", &N);
-    Nd = N * (N-3)/2;
-    printf("O número de diagonais é de: %0.2f", Nd);
-    return 0;
-}
