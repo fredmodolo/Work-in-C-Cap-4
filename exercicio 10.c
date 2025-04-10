b769e515c5e4447aaf7ff9db2a5249b7ffd59a80
Frederico Modolo <fredericomodolo@gmail.com>
2025-04-10 16:51:50 -0300

    Delete exercicio 10.c

--- exercicio 10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    float lado, resultado;
-    printf("Digite o valor dos lados do quadradado: ");
-    scanf("%f", &lado);
-    resultado = lado * lado;
-    printf("A área do quadrado será de: %0.2f\n", resultado);
-    return 0;
-}
