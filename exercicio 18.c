43b7d1408ed74af8958912f0665716c3e258f586
Frederico Modolo <fredericomodolo@gmail.com>
2025-04-10 16:53:05 -0300

    Delete exercicio 18.c

--- exercicio 18.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    float celsius, fahrenheit;
-    printf("Digite a temperatura em Celsius: ");
-    scanf("%f", &celsius);
-    fahrenheit = 180*(celsius+32)/100;
-    printf("A temperatura digitada em Fahrenheit é: %0.2f", fahrenheit);
-    return 0;
-}
