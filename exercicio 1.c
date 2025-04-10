c695987194eac9ae38ca2864035b0e7768f3f52d
Frederico Modolo <fredericomodolo@gmail.com>
2025-04-10 16:50:15 -0300

    Update exercicio 1.c

--- exercicio 1.c
+++ exercicio 1.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-int main () {
-    float n1, n2, resultado;
-    printf("Digite dois números: ");
-    scanf("%f", &n1);
-    scanf("%f", &n2);
-    resultado = (n1 - n2);
-    printf("O resultado é de: %0.1f\n", resultado);
-    return 0;
-}
+
