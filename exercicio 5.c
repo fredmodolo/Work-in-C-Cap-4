9f529c1464d3838e421c5d809eaf712bcf9b3450
Frederico Modolo <fredericomodolo@gmail.com>
2025-04-10 16:54:24 -0300

    Delete exercicio 5.c

--- exercicio 5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main () {
-    float preco, precofinal;
-    printf("Digite o valor do produto: ");
-    scanf("%f", &preco);
-    precofinal = preco - (preco*0.1);
-    printf("O valor após o desconto é de: %.2f\n", precofinal);
-    return 0;
-}
